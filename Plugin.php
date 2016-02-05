ac240fc8e4555d0ab6c4ff2173a8a84fb0de91b1
root <root@9bf442b2d162>
2016-02-05 12:19:41 +0000

    Initial commit of the BlogArchive module

---
+++ Plugin.php
@@ -0,0 +1,83 @@
+<?php namespace Graker\BlogArchive;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * BlogArchive Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+  /**
+   * @var array plugin dependencies
+   */
+  public $require = ['Rainlab.Blog'];
+
+  /**
+   * Returns information about this plugin.
+   *
+   * @return array
+   */
+  public function pluginDetails()
+  {
+    return [
+      'name'        => 'BlogArchive',
+      'description' => 'Provides archive pages to display posts per month or per year in a table manner',
+      'author'      => 'Graker',
+      'icon'        => 'icon-leaf'
+    ];
+  }
+
+  /**
+   * Registers any front-end components implemented in this plugin.
+   *
+   * @return array
+   */
+  public function registerComponents()
+  {
+    return []; // Remove this line to activate
+
+    return [
+      'Graker\BlogArchive\Components\MyComponent' => 'myComponent',
+    ];
+  }
+
+  /**
+   * Registers any back-end permissions used by this plugin.
+   *
+   * @return array
+   */
+  public function registerPermissions()
+  {
+    return []; // Remove this line to activate
+
+    return [
+      'graker.blogarchive.some_permission' => [
+        'tab' => 'BlogArchive',
+        'label' => 'Some permission'
+      ],
+    ];
+  }
+
+  /**
+   * Registers back-end navigation items for this plugin.
+   *
+   * @return array
+   */
+  public function registerNavigation()
+  {
+    return []; // Remove this line to activate
+
+    return [
+      'blogarchive' => [
+        'label'       => 'BlogArchive',
+        'url'         => Backend::url('graker/blogarchive/mycontroller'),
+        'icon'        => 'icon-leaf',
+        'permissions' => ['graker.blogarchive.*'],
+        'order'       => 500,
+      ],
+    ];
+  }
+
+}
